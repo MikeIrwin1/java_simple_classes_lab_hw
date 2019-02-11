a77eea28608248a6a573e352a8f9e204faa539d8
Michael Irwin <mike.irwin1@gmail.com>
2019-02-11 14:51:50 +0000

    waterbottle guard in place for drinking function implemented

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -11,7 +11,12 @@ public class WaterBottle {
     }
 
     public void takeDrink(){
-        this.volume -= 10;
+        if (this.volume == 0){
+            return;
+        }
+        else {
+            this.volume -= 10;
+        }
     }
 
     public void empty(){
