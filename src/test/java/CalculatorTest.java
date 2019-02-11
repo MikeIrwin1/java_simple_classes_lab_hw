1f8c78630b9606a732679de4731df3832cefe115
Michael Irwin <mike.irwin1@gmail.com>
2019-02-11 14:28:47 +0000

    add function for calculator complete

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd(){
+        assertEquals(9, calculator.add(4,5));
+    }
+
+}
